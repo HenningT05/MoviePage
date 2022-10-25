1314c879241cc0298e65cd8aa729b5e1d141483a
HenningT05 <73030011+HenningT05@users.noreply.github.com>
2022-10-25 23:13:29 +0200

    25.10 2

--- MovieData.js
+++ MovieData.js
@@ -1,7 +1,10 @@
 class Movie {
-    constructor(MovieName, ImageTag, WTW_Netflix, WTW_HBO, WTW_Viaplay, WTW_Amazone,Age_Restriction, Ganera_Number, Ganera1, Ganera2, Ganera3, MovieText, MovieDirectorImg, MovieDirectorName, Actor1IMG, Actor1Name, Actor2IMG, Actor2Name, Actor3IMG, Actor3Name, Actor4IMG, Actor4Name, Actor5IMG, Actor5Name, Actor6IMG, Actor6Name, Actor7IMG, Actor7Name, Actor8IMG, Actor8Name,Actor9IMG, Actor9Name, Actor10IMG, Actor10Name) {
+    constructor(MovieName, ImageTag,Lenght_Hours,Lenght_Minuts, Year, WTW_Netflix, WTW_HBO, WTW_Viaplay, WTW_Amazone,Age_Restriction, Ganera_Number, Ganera1, Ganera2, Ganera3, IMDB_Rating, MovieDirectorImg, MovieDirectorName, Actor1IMG, Actor1Name, Actor2IMG, Actor2Name, Actor3IMG, Actor3Name, Actor4IMG, Actor4Name, Actor5IMG, Actor5Name, Actor6IMG, Actor6Name) {
         this.MovieName = MovieName;
         this.ImageTag = ImageTag;
+        this.Lenght_Hours = Lenght_Hours;
+        this.Lenght_Minuts = Lenght_Minuts;
+        this.Year = Year;
         this.WTW_Netflix = WTW_Netflix;
         this.WTW_HBO = WTW_HBO;
         this.WTW_Viaplay = WTW_Viaplay; 
@@ -11,7 +14,7 @@ class Movie {
         this.Ganera1 = Ganera1;
         this.Ganera2 = Ganera2;
         this.Ganera3 = Ganera3;
-        this.MovieText = MovieText;
+        this.IMDB_Rating = IMDB_Rating;
         this.MovieDirectorImg = MovieDirectorImg;
         this.MovieDirectorName = MovieDirectorName;
         this.Actor1IMG = Actor1IMG;
@@ -26,27 +29,70 @@ class Movie {
         this.Actor5Name = Actor5Name;
         this.Actor6IMG = Actor6IMG;
         this.Actor6Name = Actor6Name;
-        this.Actor7IMG = Actor7IMG
-        this.Actor7Name = Actor7Name;
-        this.Actor8IMG = Actor8IMG;
-        this.Actor8Name = Actor8Name;
-        this.Actor9IMG = Actor9IMG
-        this.Actor9Name = Actor9Name;
-        this.Actor10IMG = Actor10IMG;
-        this.Actor10Name = Actor10Name;
     }
 }
 
 let FightClub = new Movie (
-"FightClub",
-"Imge tag",
-false,
-true,
-false,
-true,
-15,
-2,
-"MindFuck",
-"Action",
-"U"
+    "Fight Club",
+    "Imge tag",
+    2,
+    19,
+    1999,
+    false,
+    true,
+    false,
+    true,
+    15,
+    2,
+    "MindFuck",
+    "Action",
+    "U",
+    8.8,
+    "David Fincher iamge",
+    "David Fincher",
+    "Brad Pitt image",
+    "Brad Pitt",
+    "Edward Norton Image",
+    "Edward Norton",
+    "Helena Bonham Carter image",
+    "Helena Bonham Carter",
+    "Meat Loaf image",
+    "Meat Loaf",
+    "Meat Loaf image", 
+    "Meat Loaf",
+    "David Andrews image",
+    "David Andrews"
 );
+
+let SpiderMan_FarFormHome = new Movie (
+    "Spider-Man: Far from Home",
+    "Imge tag",
+    2,
+    9,
+    2019,
+    false,
+    true,
+    false,
+    true,
+    12,
+    2,
+    "MindFuck",
+    "Action",
+    "U",
+    7.4,
+    "Jon Watts iamge",
+    "Jon Watts",
+    "Tom Holland image",
+    "Tom Holland",
+    "Samuel L. Jackson Image",
+    "Samuel L. Jackson",
+    "Jake Gyllenhaal image",
+    "Jake Gyllenhaal",
+    "Marisa Tomei image",
+    "Marisa Tomei",
+    "Jon Favreau image", 
+    "Jon Favreau",
+    "Zendaya image",
+    "Zendaya"
+);
+    
