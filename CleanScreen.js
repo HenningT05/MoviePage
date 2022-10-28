3580697212a10af386761acce8715b8ff0365c53
HenningT05 <73030011+HenningT05@users.noreply.github.com>
2022-10-28 15:45:32 +0200

    28.10 1

--- CleanScreen.js
+++ CleanScreen.js
@@ -1,11 +1,16 @@
 var MainScreenOppend = true
 
+
+
 function CleanScreen() {
+    if(MainScreenOppend == false){
+        document.getElementsByClassName("MainIndex")[0].style.visibility="visible";
+        document.getElementsByClassName("MoviePage")[0].style.visibility="hidden"; 
+    }
     if(MainScreenOppend == true){
         document.getElementsByClassName("MainIndex")[0].style.visibility="hidden"; 
+        document.getElementsByClassName("MoviePage")[0].style.visibility="visible"; 
         MainScreenOppend = false
     }
-    else{
-        document.getElementsByClassName("MainIndex")[0].style.visibility="visible";
-    }
+   
 }
